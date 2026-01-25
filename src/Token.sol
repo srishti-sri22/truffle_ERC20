a044e7702b0ed6f0ceefb15ec183798b7488b4f5
Srishti Srivastava <srishtisri224@gmail.com>
2026-01-25 12:56:52 +0000

    add naming convention for gas optimization

--- src/Token.sol
+++ src/Token.sol
@@ -3,15 +3,23 @@
 pragma solidity ^0.8.2;
 
 contract Token {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
+    //now lets do some gas optimisations
+    //private make-> make getters
+    //immutable i_
+    //storage s_
+    //constants
 
-    mapping(address => uint256) private balances;
-    mapping(address => mapping(address => uint256)) private allowances;
+    uint8 public constant DECIMALS = 18;
 
-    address public owner;
+    string private s_name;
+    string private s_symbol;
+
+    uint256 private s_totalSupply;
+
+    mapping(address => uint256) private s_balances;
+    mapping(address => mapping(address => uint256)) private s_allowances;
+
+    address private immutable i_owner;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(
@@ -26,25 +34,55 @@ contract Token {
     }
 
     constructor(
-        string memory _name,
-        string memory _symbol,
-        uint8 _decimals,
-        uint256 _initialSupply
+        string memory name_,
+        string memory symbol_,
+        uint256 initialSupply_
     ) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        owner = msg.sender;
+        s_name = name_;
+        s_symbol = symbol_;
+        i_owner = msg.sender;
+        _mint(msg.sender, initialSupply_);
+    }
+
+    //lets make the getter functions
+
+    function name() external view returns (string memory) {
+        return s_name;
+    }
+
+    function symbol() external view returns (string memory) {
+        return s_symbol;
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return s_totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return s_balances[account];
+    }
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256) {
+        return s_allowances[owner][spender];
+    }
+
+    function get_owner() external view returns (address) {
+        return i_owner;
     }
 
     //lets make the functions that are given by the interface of OpenZepplin
 
-    function balanceOf(address account) external view returns (uint256) {
-        return balances[account];
-    }
-
     function transfer(address to, uint256 amount) external returns (bool) {
         _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        s_allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
         return true;
     }
 
@@ -53,36 +91,27 @@ contract Token {
         address to,
         uint256 amount
     ) external returns (bool) {
-        uint256 currentAllowance = allowances[from][msg.sender];
+        uint256 currentAllowance = s_allowances[from][msg.sender];
         require(currentAllowance >= amount, "Allowance exceeded");
 
-        allowances[from][msg.sender] = currentAllowance - amount;
+        unchecked {
+            s_allowances[from][msg.sender] = currentAllowance - amount;
+        }
+
         _transfer(from, to, amount);
-
         return true;
     }
-
-    function approve(address spender, uint256 amount) external returns (bool) {
-        allowances[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-
-    function allowance(
-        address tokenOwner,
-        address spender
-    ) external view returns (uint256) {
-        return allowances[tokenOwner][spender];
-    }
-
 
     //make the main comoutation functions
     function _transfer(address from, address to, uint256 amount) internal {
         require(to != address(0), "Transfer to zero address");
-        require(balances[from] >= amount, "Balance too low");
+        uint256 fromBalance = s_balances[from];
+        require(fromBalance >= amount, "Balance too low");
 
-        balances[from] -= amount;
-        balances[to] += amount;
+        unchecked {
+            s_balances[from] = fromBalance - amount;
+            s_balances[to] += amount;
+        }
 
         emit Transfer(from, to, amount);
     }
@@ -90,15 +119,14 @@ contract Token {
     function _mint(address to, uint256 amount) internal {
         require(to != address(0), "Mint to zero address");
 
-        totalSupply += amount;
-        balances[to] += amount;
+        s_totalSupply += amount;
+        s_balances[to] += amount;
 
         emit Transfer(address(0), to, amount);
     }
 
-
     //now lets make the final and main minting function, for the faucet
-     function mint(address to, uint256 amount) external onlyOwner {
+    function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 }
